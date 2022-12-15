fc8c58fcf0a77244b79d5f255c4a9479812dcee9
Paul Samosha <pasamoshin@gmail.com>
2022-12-15 23:10:39 +0300

    psycopg creat

---
+++ hw_db.py
@@ -0,0 +1,110 @@
+import psycopg2
+
+
+def drop_table(cur, *table):
+    for t in table:
+        cur.execute(f"DROP TABLE {t}")
+
+def create_db(cur):
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS clients(
+                    id SERIAL PRIMARY KEY,
+                    first_name VARCHAR(40) NOT NULL,
+                    last_name VARCHAR(40) NOT NULL,
+                    email VARCHAR(40) UNIQUE
+                );
+                CREATE TABLE IF NOT EXISTS clients_phones(
+                    id SERIAL PRIMARY KEY,
+                    id_client INTEGER NOT NULL REFERENCES clients(id),
+                    phones VARCHAR(14)
+                );
+                """)
+
+
+def add_client(cur, first_name, last_name, email, phones=None):
+    cur.execute("""
+                INSERT INTO clients(first_name, last_name, email)
+                VALUES (%s, %s, %s)
+                RETURNING id;
+                """, (first_name, last_name, email))
+    id_client = cur.fetchone()
+    if phones:
+        cur.execute("""
+                    INSERT INTO clients_phones(id_client, phones)
+                    VALUES (%s, %s)
+                    """, (id_client, phones))
+
+
+def add_phone(cur, id_client, phones):
+    cur.execute("""
+                INSERT INTO clients_phones(id_client, phones)
+                VALUES (%s, %s)
+                """, (id_client, phones))
+
+
+def change_client(cur, id_client, first_name=None, last_name=None, email=None, phones=None):
+    if phones != None:
+        cur.execute("""
+            UPDATE clients_phones
+            SET phones = %s
+            WHERE client_id = %s
+            """, (phones, id_client))
+
+    if email != None:
+        cur.execute("""
+            UPDATE clients
+            SET email = %s
+            WHERE id = %s
+            """, (email, id_client))
+
+        if last_name != None:
+            cur.execute("""
+                UPDATE clients
+                SET last_name = %s
+                WHERE id = %s
+                """, (last_name, id_client))
+
+        if first_name != None:
+            cur.execute("""
+                UPDATE clients
+                SET first_name = %s
+                WHERE id = %s
+                """, (first_name, id_client))
+
+
+
+def delete_phone(cur, phone=None, client_id=None):
+    cur.execute("""
+        DELETE FROM clients_phones WHERE id=%s or phones=%s;
+        """, (client_id, phone))
+
+
+def delete_client(cur, client_id):
+    cur.execute("""
+        DELETE FROM clients_phones WHERE id=%s;
+        """, (client_id))
+    delete_phone(cur, None, client_id)
+
+
+def find_client(cur, first_name=None, last_name=None, email=None, phone=None):
+    cur.execute("""
+        SELECT c*, cp.phones FROM clients c
+        JOIN clients_phones ON c.id = cp.id_client
+        WHERE id=%s;
+        """, ())
+    delete_phone(cur, None, )
+
+
+with psycopg2.connect(database="clients_db", user="postgres", password="1234") as conn:
+    with conn.cursor() as cur:
+        drop_table(cur, 'clients_phones', 'clients')
+        create_db(cur)
+        add_client(cur, 'Ivan','Ivanov','iban@ng.ru', '+79999999999')
+        add_phone(cur, '1', '+78889999999')
+        change_client(cur,1)
+        delete_phone(cur, '+78889999999')
+        
+        conn.commit()
+
+
+# conn.close()
